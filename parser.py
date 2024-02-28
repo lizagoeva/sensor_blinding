b04a121a8b80ebbc9acb4eef470cbc3951eed109
ShdwL1ne <romo4ka.nya@gmail.com>
2024-02-28 19:10:23 +0300

    Integrate parser into main3.py

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ from random import randint
 
 
 def parse_snort_rule(rule):
+
     # Регулярное выражение для извлечения параметров из строки правила
     pattern = r'^\s*(?:(?!#).*alert\s+(?P<protocol>\S+)\s+(?P<source_address>\S+)\s+(?P<source_port>\S+)\s+->\s+(?P<destination_address>\S+)\s+(?P<destination_port>\S+)\s+\(.*content:"(?P<content>[^"]*)".*;)?'
     match = re.match(pattern, rule)
@@ -11,7 +12,7 @@ def parse_snort_rule(rule):
     else:
         return None
 
-def parse_snort_rules(rules_file, protocol, port, host):
+def parse_snort_rules(rules_file, protocol, host, port):
     parsed_rules = []
 
     # Чтение правил из файла
@@ -22,6 +23,7 @@ def parse_snort_rules(rules_file, protocol, port, host):
     for rule in rules:
         parsed_rule = parse_snort_rule(rule)
         if parsed_rule:
+            
             # Проверка по протоколу и destiination_port
             if (parsed_rule["protocol"] == protocol and
                 parsed_rule["destination_address"] == host and
@@ -46,11 +48,11 @@ def parse_snort_rules(rules_file, protocol, port, host):
     return parsed_rules
 
 # Пример использования
-rules_file = "community.rules"
-protocol = "tcp"
-port = 22
-host = "$EXTERNAL_NET"
+# rules_file = "community.rules"
+# protocol = "tcp"
+# port = 22
+# host = "$EXTERNAL_NET"
 
-filtered_rules = parse_snort_rules(rules_file, protocol, port, host)
-for rule in filtered_rules:
-    print(rule)
+# filtered_rules = parse_snort_rules(rules_file, protocol, port, host)
+# for rule in filtered_rules:
+#     print(rule)
